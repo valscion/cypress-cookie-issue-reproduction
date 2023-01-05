490830579e43299bac32f0121d6c9c2d3ab9e488
Vesa Laakso <482561+valscion@users.noreply.github.com>
2023-01-05 13:40:10 +0200

    Write assertions so they work also with Cypress pre v12

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,9 +4,23 @@ describe('issue https://github.com/cypress-io/cypress/issues/25174 reproduction'
     cy.get('#do-xhr-request').click()
     cy.contains('A cookie has been set').should('be.visible')
 
-    // This is the strange thing! There should only be one cookie set
-    cy.getAllCookies().then(cookies => cy.log("Cookie domains are:", cookies.map(c => c.domain)))
-    cy.getAllCookies().should('have.length', 1)
+    cy.get('#client-cookies-json').invoke('text').should(text => {
+      const parsedText = JSON.parse(text)
+      // There should only be one cookie here
+      expect(parsedText).to.have.length(1)
+    });
+
+    // Debug with cy.getAllCookies if that exists (Cypress v12.1.0+ only)
+    if (cy.getAllCookies) {
+      cy.getAllCookies().then(cookies => cy.log("Cookie domains are:", cookies.map(c => c.domain)))
+    }
+
+    cy.get('#client-cookies-json-timeout').invoke('text').should(text => {
+      const parsedText = JSON.parse(text)
+      // This is the strange thing!
+      // There should only be one cookie here, too
+      expect(parsedText).to.have.length(1)
+    });
   })
 
   it('creates two cookies with a fetch request', () => {
@@ -14,8 +28,22 @@ describe('issue https://github.com/cypress-io/cypress/issues/25174 reproduction'
     cy.get('#do-fetch-request').click()
     cy.contains('A cookie has been set').should('be.visible')
 
-    // This is the strange thing! There should only be one cookie set
-    cy.getAllCookies().then(cookies => cy.log("Cookie domains are:", cookies.map(c => c.domain)))
-    cy.getAllCookies().should('have.length', 1)
+    cy.get('#client-cookies-json').invoke('text').should(text => {
+      const parsedText = JSON.parse(text)
+      // There should only be one cookie here
+      expect(parsedText).to.have.length(1)
+    });
+
+    // Debug with cy.getAllCookies if that exists (Cypress v12.1.0+ only)
+    if (cy.getAllCookies) {
+      cy.getAllCookies().then(cookies => cy.log("Cookie domains are:", cookies.map(c => c.domain)))
+    }
+
+    cy.get('#client-cookies-json-timeout').invoke('text').should(text => {
+      const parsedText = JSON.parse(text)
+      // This is the strange thing!
+      // There should only be one cookie here, too
+      expect(parsedText).to.have.length(1)
+    });
   })
 })
